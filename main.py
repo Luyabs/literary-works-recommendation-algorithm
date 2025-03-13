d0b52e38a0393570ee50ce7a4bb55bd0e4e42d9f
abstraction <2830391724@qq.com>
2025-03-13 15:59:01 +0800

    main文件用于Linux上更方便的训练模型：PYTHONPATH="/.../literary_works_rec" conda run -n literay-works-rec --no-capture-output python /.../literary_works_rec/main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import os.path
+
+from model_and_train.multi_feature_fm.multi_feature_fm_trainer_and_predictor import MultiFeatureFMSystem
+
+if __name__ == '__main__':
+    script_dir = os.path.dirname(os.path.abspath(__file__))  # 获取当前脚本所在目录
+    bert_path = os.path.join(script_dir, os.path.join("model_and_train", "multi_feature_fm", "base_bert_chinese"))
+
+    multi_feature_fm_system = MultiFeatureFMSystem(bert_version=bert_path)
+    # multi_feature_fm_system.train(dataloader=multi_feature_fm_system.train_dataloader)
+    multi_feature_fm_system.test(dataloader=multi_feature_fm_system.test_dataloader)
